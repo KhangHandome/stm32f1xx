993c177ca2cca08fc4edb8f16855c825f2c5f351
DESKTOP-EIS1D5L\KhangMT <maithanhkhang99@gmail.com>
2025-06-01 22:55:09 +0700

    Update code

---
+++ coding/uart/Debug/Src/main.d
@@ -0,0 +1,2 @@
+Src/main.o: ../Src/main.c ../Inc/hal_uart.h
+../Inc/hal_uart.h:
---
+++ coding/uart/Inc/hal_uart.h
@@ -0,0 +1,17 @@
+/*
+ * hal_uart.h
+ *
+ *  Created on: Jun 1, 2025
+ *      Author: KhangMT
+ */
+
+#ifndef HAL_UART_H_
+#define HAL_UART_H_
+extern void HAL_UART_Init();
+extern void HAL_UART_SendChar();
+extern void HAL_UART_SendString();
+extern void HAL_UART_RevString();
+extern void HAL_UART_Deinit();
+
+
+#endif /* HAL_UART_H_ */
